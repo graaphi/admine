9d72ca71a37451ceb3003d7d339973dd5a08a9fe
Pedro Palau <pepalauisaac@gmail.com>
2020-07-07 00:47:32 -0500

    Adds `mdx` provider to main application

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { MDXProvider } from '@mdx-js/react';
 
 import Head from 'next/head';
 
@@ -20,7 +21,7 @@ const App = ({
   Component,
   pageProps,
 }) => (
-  <>
+  <MDXProvider>
     <Head>
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       <link rel="icon" href="/favicon.ico" />
@@ -28,7 +29,7 @@ const App = ({
     <Component
       {...pageProps}
     />
-  </>
+  </MDXProvider>
 );
 
 App.defaultProps = defaultProps;
