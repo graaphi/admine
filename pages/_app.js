2e60c1189108f4bfce0246757b5bc8b09929d360
Pedro Palau <pepalauisaac@gmail.com>
2020-07-08 23:21:12 -0500

    Updates application head metadata and favicons

--- pages/_app.js
+++ pages/_app.js
@@ -23,9 +23,12 @@ const App = ({
 }) => (
   <MDXProvider>
     <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     </Head>
     <Component
       {...pageProps}
