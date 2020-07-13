aea8ef06f310ba87e464d35a0baa3f9d6c271586
Pedro Palau <pepalauisaac@gmail.com>
2020-07-13 16:07:35 -0500

    Creates a loading spinner

--- components/LoadingIndicator/LoadingIndicator.js
+++ components/LoadingIndicator/LoadingIndicator.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+import Head from '../Head';
 import Logo from '../Logo';
 
 const LoadingIndicator = () => (
-  <div className="flex justify-center items-center min-h-full w-full bg-purple-900 text-white space-x-4">
-    <Logo />
-    <span className="text-purple-400">|</span>
-    <h4 className="font-normal text-sm">
-      Loading...
-    </h4>
-  </div>
+  <>
+    <Head title="Loading" />
+    <div className="flex justify-center items-center min-h-screen w-full bg-purple-900 text-white space-x-4">
+      <Logo />
+      <span className="text-purple-400">|</span>
+      <div className="spinner" />
+      <h4 className="font-normal text-sm">
+        Loading...
+      </h4>
+    </div>
+  </>
 );
 
 export default LoadingIndicator;
